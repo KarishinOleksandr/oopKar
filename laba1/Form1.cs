55fa600c383830ca76078e23af544b3e5b03c1d7
skarishin12345@gmail.com <skarishin12345@gmail.com>
2024-02-12 22:15:09 +0200

    дороблення "перевірки на ідіота"

--- laba1/Form1.cs
+++ laba1/Form1.cs
@@ -64,9 +64,16 @@ namespace laba1
 
         private void btnOk_Click(object sender, EventArgs e)
         {
-            double x = Convert.ToDouble(txtX.Text);
-            double result = (Math.Log(Math.Abs(Math.Cos(x))) / Math.Log(1 + Math.Pow(x, 2)));
-            lblResult.Text = result.ToString();
+           if(txtX.TextLength != 0)
+            {
+                double x = Convert.ToDouble(txtX.Text);
+                double result = (Math.Log(Math.Abs(Math.Cos(x))) / Math.Log(1 + Math.Pow(x, 2)));
+                lblResult.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Input data!");
+            }
 
         }
 
@@ -74,7 +81,11 @@ namespace laba1
         {
             if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
             { return; }
-            e.Handled = true;
+            if (e.KeyChar == '+' || e.KeyChar == '-')
+            { return; }
+            if (e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Delete)
+            { return; }
+            e.Handled = true; ;
         }
 
         private void tabPage1_Click(object sender, EventArgs e)
@@ -89,11 +100,18 @@ namespace laba1
 
         private void btnOk2_Click(object sender, EventArgs e)
         {
-            double a = Convert.ToDouble(txtA.Text);
-            double b = Convert.ToDouble(txtB.Text);
-            double y = Convert.ToDouble(txty.Text);
-            double result2 = (((a * b) / 2) * (Math.Sin(y * Math.PI / 180)));
-            lblResult2.Text = result2.ToString();
+            if (txtA.TextLength != 0 || txtB.TextLength !=0 || txty.TextLength != 0)
+            {
+                double a = Convert.ToDouble(txtA.Text);
+                double b = Convert.ToDouble(txtB.Text);
+                double y = Convert.ToDouble(txty.Text);
+                double result2 = (((a * b) / 2) * (Math.Sin(y * Math.PI / 180)));
+                lblResult2.Text = result2.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Input data!");
+            }
         }
 
         private void txtA_TextChanged(object sender, EventArgs e)
@@ -115,14 +133,18 @@ namespace laba1
         {
             if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
             { return; }
+            if (e.KeyChar == '+' || e.KeyChar == '-')
+            { return; }
             if (e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Delete)
             { return; }
-                e.Handled = true;
+            e.Handled = true;
         }
 
         private void txtB_KeyPress(object sender, KeyPressEventArgs e)
         {
             if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            { return; }
+            if (e.KeyChar == '+' || e.KeyChar == '-')
             { return; }
             if (e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Delete)
             { return; }
@@ -133,6 +155,8 @@ namespace laba1
         {
             if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
             { return; }
+            if (e.KeyChar == '+' || e.KeyChar == '-')
+            { return; }
             if (e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Delete)
             { return; }
             e.Handled = true;
@@ -140,19 +164,26 @@ namespace laba1
 
         private void btnOK3_Click(object sender, EventArgs e)
         {
-            double aa = Convert.ToDouble(txtaa.Text);
-            double bb = Convert.ToDouble(txtbb.Text);
-            double c = Convert.ToDouble(txtcc.Text);
-            double d = Convert.ToDouble(txtdd.Text);
-            double check1 = (aa / c);
-            double check2 = (bb / d);
-            double check3 = (aa / d);
-            double check4 = (bb / c);
-            if (check1 == check2)
-            { lblResult3.Text = "true"; }
-            else if (check3 == check4)
-            { lblResult3.Text = "true"; }
-            else { lblResult3.Text = "false"; }
+            if (txtaa.TextLength !=0 || txtbb.TextLength !=0 || txtcc.TextLength !=0 || txtdd.TextLength != 0)
+            {
+                double aa = Convert.ToDouble(txtaa.Text);
+                double bb = Convert.ToDouble(txtbb.Text);
+                double c = Convert.ToDouble(txtcc.Text);
+                double d = Convert.ToDouble(txtdd.Text);
+                double check1 = (aa / c);
+                double check2 = (bb / d);
+                double check3 = (aa / d);
+                double check4 = (bb / c);
+                if (check1 == check2)
+                { lblResult3.Text = "true"; }
+                else if (check3 == check4)
+                { lblResult3.Text = "true"; }
+                else { lblResult3.Text = "false"; }
+            }
+            else
+            {
+                MessageBox.Show("Input data!");
+            }
             
         }
 
@@ -165,6 +196,8 @@ namespace laba1
         {
             if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
             { return; }
+            if (e.KeyChar == '+' || e.KeyChar == '-')
+            { return; }
             if (e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Delete)
             { return; }
             e.Handled = true;
@@ -173,6 +206,8 @@ namespace laba1
         private void txtbb_KeyPress(object sender, KeyPressEventArgs e)
         {
             if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            { return; }
+            if (e.KeyChar == '+' || e.KeyChar == '-')
             { return; }
             if (e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Delete)
             { return; }
@@ -183,6 +218,8 @@ namespace laba1
         {
             if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
             { return; }
+            if (e.KeyChar == '+' || e.KeyChar == '-')
+            { return; }
             if (e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Delete)
             { return; }
             e.Handled = true;
@@ -192,6 +229,8 @@ namespace laba1
         {
             if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
             { return; }
+            if (e.KeyChar == '+' || e.KeyChar == '-')
+            { return; }
             if (e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Delete)
             { return; }
             e.Handled = true;
@@ -199,16 +238,21 @@ namespace laba1
 
         private void btnOK4_Click(object sender, EventArgs e)
         {
-            double first = Convert.ToDouble(txtfirst.Text);
-            double second = Convert.ToDouble(txtsecond.Text);
-            double third = Convert.ToDouble(txtthird.Text);
-            bool Positive = (first + second > 0) ||(first + third > 0) || (second + third > 0);
-            if (Positive)
+            if(txtfirst.TextLength != 0 || txtsecond.TextLength !=0 || txtthird.TextLength != 0)
             {
-                lblResult4.Text = "Sum of one is positive";
+                double first = Convert.ToDouble(txtfirst.Text);
+                double second = Convert.ToDouble(txtsecond.Text);
+                double third = Convert.ToDouble(txtthird.Text);
+                bool Positive = (first + second > 0) || (first + third > 0) || (second + third > 0);
+                if (Positive)
+                {
+                    lblResult4.Text = "Sum of one is positive";
+                }
             }
-            else 
-            { lblResult4.Text = "Sum of any is negative"; }
+            else
+            {
+                MessageBox.Show("Input data!");
+            }
         }
 
         private void txtfirst_KeyPress(object sender, KeyPressEventArgs e)
@@ -281,42 +325,49 @@ namespace laba1
         private void btnOK6_Click(object sender, EventArgs e)
         {
 
-            int a1 = int.Parse(txta6_1.Text);
-            int an = int.Parse(txta6_2.Text);
-            int b1 = int.Parse(txtb6_1.Text);
-            int bn = int.Parse(txtb6_2.Text);
-
-            int[] sequenceA = GenerateSequence(a1, an);
-            int[] sequenceB = GenerateSequence(b1, bn);
-
-            TransformSequence(sequenceA, sequenceB);
-            for (int i = 0; i < sequenceB.Length; i++)
+            if(txta6_1.TextLength != 0 ||  txta6_2.TextLength != 0 || txtb6_1.TextLength !=0 || txtb6_2.TextLength != 0)
             {
-                lblResult6.Text = ($"b{i + 1} = {sequenceB[i]}");
-            }
+                int a1 = int.Parse(txta6_1.Text);
+                int an = int.Parse(txta6_2.Text);
+                int b1 = int.Parse(txtb6_1.Text);
+                int bn = int.Parse(txtb6_2.Text);
 
-            int[] GenerateSequence(int start, int end)
-            {
-                int[] sequence = new int[Math.Abs(end - start) + 1];
-                for (int i = 0; i < sequence.Length; i++)
+                int[] sequenceA = GenerateSequence(a1, an);
+                int[] sequenceB = GenerateSequence(b1, bn);
+
+                TransformSequence(sequenceA, sequenceB);
+                for (int i = 0; i < sequenceB.Length; i++)
                 {
-                    sequence[i] = start + i;
+                    lblResult6.Text = ($"b{i + 1} = {sequenceB[i]}");
                 }
-                return sequence;
-            }
-            void TransformSequence(int[] a, int[] b)
-            {
-                for (int i = 0; i < a.Length && i < b.Length; i++)
+
+                int[] GenerateSequence(int start, int end)
                 {
-                    if (a[i] <= 0)
+                    int[] sequence = new int[Math.Abs(end - start) + 1];
+                    for (int i = 0; i < sequence.Length; i++)
                     {
-                        b[i] *= 10;
+                        sequence[i] = start + i;
                     }
-                    else
+                    return sequence;
+                }
+                void TransformSequence(int[] a, int[] b)
+                {
+                    for (int i = 0; i < a.Length && i < b.Length; i++)
                     {
-                        b[i] = 0;
+                        if (a[i] <= 0)
+                        {
+                            b[i] *= 10;
+                        }
+                        else
+                        {
+                            b[i] = 0;
+                        }
                     }
                 }
+            }
+            else
+            {
+                MessageBox.Show("Input data!");
             }
             
         }
@@ -367,37 +418,44 @@ namespace laba1
 
         private void btnOK7_Click(object sender, EventArgs e)
         {
-            string input_txt = txt7.Text;
-
-            int count_abc = CountABCGroups(input_txt);
-
-            string result = Convert.ToString(count_abc);
-
-            lblResult7.Text = result;
-
-            int CountABCGroups(string input)
+            if (txt7.TextLength != 0)
             {
-                int count = 0;
+                string input_txt = txt7.Text;
 
-                for (int i = 0; i < input.Length; i++)
+                int count_abc = CountABCGroups(input_txt);
+
+                string result = Convert.ToString(count_abc);
+
+                lblResult7.Text = result;
+
+                int CountABCGroups(string input)
                 {
-                    if (input[i] == 'a')
+                    int count = 0;
+
+                    for (int i = 0; i < input.Length; i++)
                     {
-                        count++;
+                        if (input[i] == 'a')
+                        {
+                            count++;
+
+                        }
+                        else if (input[i] == 'b')
+                        {
+                            count++;
+                        }
+                        else if (input[i] == 'c')
+                        {
+                            count++;
+                        }
 
                     }
-                    else if (input[i] == 'b')
-                    {
-                        count ++;
-                    }
-                    else if (input[i] == 'c')
-                    {
-                        count ++;
-                    }
-                    
+
+                    return count;
                 }
-
-                return count;
+            }
+            else
+            {
+                MessageBox.Show("Input data!");
             }
         }
     }
