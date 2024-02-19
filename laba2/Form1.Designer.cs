bd88e57a7fe6e1e7d0c6b8854033e9d3c385763d
skarishin12345@gmail.com <skarishin12345@gmail.com>
2024-02-20 00:24:26 +0200

    50% of work (or not)

---
+++ laba2/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace laba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtdegree = new System.Windows.Forms.TextBox();
+            this.txtcoef = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(80, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(425, 116);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 316);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(109, 364);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(244, 364);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "label5";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(247, 443);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 6;
+            this.btnOK.Text = "button1";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtdegree
+            // 
+            this.txtdegree.Location = new System.Drawing.Point(247, 228);
+            this.txtdegree.Name = "txtdegree";
+            this.txtdegree.Size = new System.Drawing.Size(100, 20);
+            this.txtdegree.TabIndex = 7;
+            // 
+            // txtcoef
+            // 
+            this.txtcoef.Location = new System.Drawing.Point(247, 267);
+            this.txtcoef.Name = "txtcoef";
+            this.txtcoef.Size = new System.Drawing.Size(100, 20);
+            this.txtcoef.TabIndex = 8;
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(247, 309);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 20);
+            this.txtX.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1458, 648);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.txtcoef);
+            this.Controls.Add(this.txtdegree);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox txtdegree;
+        private System.Windows.Forms.TextBox txtcoef;
+        private System.Windows.Forms.TextBox txtX;
+    }
+}
+
---
+++ laba2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Ступінь многочлена";
+            label2.Text = "Коефіціент многочленів(чeрез кому)";
+            label3.Text = "x = ";
+            label4.Text = "Result";
+            lblResult.Text = "";
+            btnOK.Text = "Generate";
+        }
+
+        public void btnOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double degree = Convert.ToDouble(txtdegree.Text);
+                string[] coefString = txtcoef.Text.Split(',');
+                double[] coef = new double[coefString.Length];
+
+                for (int i = 0; i < coefString.Length; i++)
+                {
+                    coef[i] = Convert.ToDouble(coefString[i]);
+                }
+
+                double x = Convert.ToDouble(txtX.Text);
+
+                Polynomial polynomial = new Polynomial(degree, coef);
+                double result = polynomial.Evaluate(x);
+
+                lblResult.Text = result.ToString();
+
+            } 
+            catch (Exception ex) 
+            {
+                lblResult.Text = "Помилка: " + ex.Message;
+            }
+          
+        }
+
+        public class Polynomial
+        {
+            private double degree;
+            private double[] coef;
+
+            public Polynomial(double degree, double[] coef)
+            {
+                this.degree = degree;
+                this.coef = coef;
+            }
+
+            public double Evaluate(double x)
+            {
+                double result = 0;
+
+                for (int i = 0; i <= degree && i < coef.Length; i++)
+                {
+                    result += coef[i] * Math.Pow(x, i);
+                }
+
+                return result;
+            }
+        }
+    }
+}
