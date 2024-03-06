d9039d5dfd9d4a8489203b2640746d7b96ccd795
skarishin12345@gmail.com <skarishin12345@gmail.com>
2024-03-06 21:14:14 +0200

    50% of work (laba3)

---
+++ laba3/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnmulty = new System.Windows.Forms.Button();
+            this.btndivide = new System.Windows.Forms.Button();
+            this.btnminus = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btnplus = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1411, 603);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txt4);
+            this.tabPage1.Controls.Add(this.txt3);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.btnplus);
+            this.tabPage1.Controls.Add(this.btnmulty);
+            this.tabPage1.Controls.Add(this.btndivide);
+            this.tabPage1.Controls.Add(this.btnminus);
+            this.tabPage1.Controls.Add(this.lblResult);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.txt2);
+            this.tabPage1.Controls.Add(this.txt1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1403, 577);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // btnmulty
+            // 
+            this.btnmulty.Location = new System.Drawing.Point(782, 408);
+            this.btnmulty.Name = "btnmulty";
+            this.btnmulty.Size = new System.Drawing.Size(75, 23);
+            this.btnmulty.TabIndex = 10;
+            this.btnmulty.Text = "button4";
+            this.btnmulty.UseVisualStyleBackColor = true;
+            this.btnmulty.Click += new System.EventHandler(this.btnmulty_Click);
+            // 
+            // btndivide
+            // 
+            this.btndivide.Location = new System.Drawing.Point(601, 408);
+            this.btndivide.Name = "btndivide";
+            this.btndivide.Size = new System.Drawing.Size(75, 23);
+            this.btndivide.TabIndex = 9;
+            this.btndivide.Text = "button3";
+            this.btndivide.UseVisualStyleBackColor = true;
+            this.btndivide.Click += new System.EventHandler(this.btndivide_Click);
+            // 
+            // btnminus
+            // 
+            this.btnminus.Location = new System.Drawing.Point(431, 408);
+            this.btnminus.Name = "btnminus";
+            this.btnminus.Size = new System.Drawing.Size(75, 23);
+            this.btnminus.TabIndex = 8;
+            this.btnminus.Text = "button2";
+            this.btnminus.UseVisualStyleBackColor = true;
+            this.btnminus.Click += new System.EventHandler(this.btnminus_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(258, 363);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(261, 292);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 4;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(261, 238);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(111, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(886, 118);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1403, 577);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1403, 577);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnplus
+            // 
+            this.btnplus.Location = new System.Drawing.Point(261, 407);
+            this.btnplus.Name = "btnplus";
+            this.btnplus.Size = new System.Drawing.Size(75, 23);
+            this.btnplus.TabIndex = 11;
+            this.btnplus.Text = "button1";
+            this.btnplus.UseVisualStyleBackColor = true;
+            this.btnplus.Click += new System.EventHandler(this.btnplus_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(470, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(473, 292);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(601, 230);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 14;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(601, 285);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 20);
+            this.txt4.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1435, 627);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnmulty;
+        private System.Windows.Forms.Button btndivide;
+        private System.Windows.Forms.Button btnminus;
+        private System.Windows.Forms.Button btnplus;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ laba3/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static laba3.Form1;
+
+namespace laba3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public abstract class Pair
+        {
+            public abstract Pair Add(Pair other);
+            public abstract Pair Subtract(Pair other);
+            public abstract Pair Multiply(Pair other);
+            public abstract Pair Divide(Pair other);
+
+            public abstract override string ToString();
+        }
+
+        public class Complex : Pair
+        {
+            public double Real { get; set; }
+            public double Imaginary { get; set; }
+
+            public Complex(double real, double imaginary)
+            {
+                Real = real;
+                Imaginary = imaginary;
+            }
+
+            public override Pair Add(Pair other)
+            {
+                if (other is Complex)
+                {
+                    var complex = (Complex)other;
+                    return new Complex(Real + complex.Real, Imaginary + complex.Imaginary);
+                }
+
+                throw new ArgumentException("Invalid type for addition.");
+            }
+
+            public override Pair Subtract(Pair other)
+            {
+                if (other is Complex)
+                {
+                    var complex = (Complex)other;
+                    return new Complex(Real - complex.Real, Imaginary - complex.Imaginary);
+                }
+
+                throw new ArgumentException("Invalid type for subtraction.");
+            }
+
+            public override Pair Multiply(Pair other)
+            {
+                if (other is Complex)
+                {
+                    var complex = (Complex)other;
+                    return new Complex(
+                        Real * complex.Real - Imaginary * complex.Imaginary,
+                        Real * complex.Imaginary + Imaginary * complex.Real
+                    );
+                }
+
+                throw new ArgumentException("Invalid type for multiplication.");
+            }
+
+            public override Pair Divide(Pair other)
+            {
+                if (other is Complex)
+                {
+                    var complex = (Complex)other;
+                    double denominator = complex.Real * complex.Real + complex.Imaginary * complex.Imaginary;
+
+                    if (denominator != 0)
+                    {
+                        double realPart = (Real * complex.Real + Imaginary * complex.Imaginary) / denominator;
+                        double imaginaryPart = (Imaginary * complex.Real - Real * complex.Imaginary) / denominator;
+
+                        return new Complex(realPart, imaginaryPart);
+                    }
+
+                    throw new DivideByZeroException("Division by zero is not allowed.");
+                }
+
+                throw new ArgumentException("Invalid type for division.");
+            }
+
+            public override string ToString()
+            {
+                return $"{Real} + {Imaginary}i";
+            }
+        }
+
+        public class Rational : Pair
+        {
+            public int Numerator { get; set; }
+            public int Denominator { get; set; }
+
+            public Rational(int numerator, int denominator)
+            {
+                if (denominator == 0)
+                    throw new ArgumentException("Denominator cannot be zero.");
+
+                int gcd = GCD(Math.Abs(numerator), Math.Abs(denominator));
+                Numerator = numerator / gcd;
+                Denominator = denominator / gcd;
+            }
+
+            private int GCD(int a, int b)
+            {
+                while (b != 0)
+                {
+                    int temp = b;
+                    b = a % b;
+                    a = temp;
+                }
+                return a;
+            }
+
+            public override Pair Add(Pair other)
+            {
+                if (other is Rational)
+                {
+                    var rational = (Rational)other;
+                    int commonDenominator = LCM(Denominator, rational.Denominator);
+                    int sumNumerator = Numerator * (commonDenominator / Denominator) +
+                                       rational.Numerator * (commonDenominator / rational.Denominator);
+
+                    return new Rational(sumNumerator, commonDenominator);
+                }
+
+                throw new ArgumentException("Invalid type for addition.");
+            }
+
+            public override Pair Subtract(Pair other)
+            {
+                if (other is Rational)
+                {
+                    var rational = (Rational)other;
+                    int commonDenominator = LCM(Denominator, rational.Denominator);
+                    int diffNumerator = Numerator * (commonDenominator / Denominator) -
+                                        rational.Numerator * (commonDenominator / rational.Denominator);
+
+                    return new Rational(diffNumerator, commonDenominator);
+                }
+
+                throw new ArgumentException("Invalid type for subtraction.");
+            }
+
+            public override Pair Multiply(Pair other)
+            {
+                if (other is Rational)
+                {
+                    var rational = (Rational)other;
+                    return new Rational(Numerator * rational.Numerator, Denominator * rational.Denominator);
+                }
+
+                throw new ArgumentException("Invalid type for multiplication.");
+            }
+
+            public override Pair Divide(Pair other)
+            {
+                if (other is Rational)
+                {
+                    var rational = (Rational)other;
+                    if (rational.Numerator != 0)
+                    {
+                        return new Rational(Numerator * rational.Denominator, Denominator * rational.Numerator);
+                    }
+
+                    throw new DivideByZeroException("Division by zero is not allowed.");
+                }
+
+                throw new ArgumentException("Invalid type for division.");
+            }
+
+            private int LCM(int a, int b)
+            {
+                return Math.Abs(a * b) / GCD(a, b);
+            }
+
+            public override string ToString()
+            {
+                return $"{Numerator}/{Denominator}";
+            }
+        }
+
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "real1";
+            label2.Text = "real2";
+            label3.Text = "Result: ";
+            lblResult.Text = "";
+            label4.Text = "imaginary1";
+            label5.Text = "imaginary2";
+        }
+
+        private void btnplus_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt1.Text, out double real1) && double.TryParse(txt3.Text, out double imaginary1) &&
+        double.TryParse(txt2.Text, out double real2) && double.TryParse(txt4.Text, out double imaginary2))
+            {
+                Pair pair1 = new Complex(real1, imaginary1);
+                Pair pair2 = new Complex(real2, imaginary2);
+                Pair resultAdd = pair1.Add(pair2);
+                lblResult.Text = resultAdd.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input data!");
+            }
+        }
+
+        private void btnminus_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt1.Text, out double real1) && double.TryParse(txt3.Text, out double imaginary1) &&
+        double.TryParse(txt2.Text, out double real2) && double.TryParse(txt4.Text, out double imaginary2))
+            {
+                Pair pair1 = new Complex(real1, imaginary1);
+                Pair pair2 = new Complex(real2, imaginary2);
+                Pair resultSub = pair1.Subtract(pair2);
+                lblResult.Text = resultSub.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input data!");
+            }
+        }
+
+        private void btndivide_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt1.Text, out double real1) && double.TryParse(txt3.Text, out double imaginary1) &&
+        double.TryParse(txt2.Text, out double real2) && double.TryParse(txt4.Text, out double imaginary2))
+            {
+                Pair pair1 = new Complex(real1, imaginary1);
+                Pair pair2 = new Complex(real2, imaginary2);
+                Pair resultDivide = pair1.Divide(pair2);
+                lblResult.Text = resultDivide.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input data!");
+            }
+        }
+
+        private void btnmulty_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt1.Text, out double real1) && double.TryParse(txt3.Text, out double imaginary1) &&
+        double.TryParse(txt2.Text, out double real2) && double.TryParse(txt4.Text, out double imaginary2))
+            {
+                Pair pair1 = new Complex(real1, imaginary1);
+                Pair pair2 = new Complex(real2, imaginary2);
+                Pair resultMult = pair1.Multiply(pair2);
+                lblResult.Text = resultMult.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input data!");
+            }
+        }
+    }
+}
