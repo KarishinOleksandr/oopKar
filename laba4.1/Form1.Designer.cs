4f9aeafcc3b0af9e9c45fe0ee976f6ec088e0257
skarishin12345@gmail.com <skarishin12345@gmail.com>
2024-03-18 23:13:02 +0200

    1 завдання лаби 4

---
+++ laba4.1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace laba4._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtArray = new System.Windows.Forms.TextBox();
+            this.btnOk1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnOk3 = new System.Windows.Forms.Button();
+            this.btnOk2 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 285);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(219, 367);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "label3";
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(222, 278);
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(100, 20);
+            this.txtArray.TabIndex = 4;
+            // 
+            // btnOk1
+            // 
+            this.btnOk1.Location = new System.Drawing.Point(222, 442);
+            this.btnOk1.Name = "btnOk1";
+            this.btnOk1.Size = new System.Drawing.Size(75, 23);
+            this.btnOk1.TabIndex = 5;
+            this.btnOk1.Text = "button1";
+            this.btnOk1.UseVisualStyleBackColor = true;
+            this.btnOk1.Click += new System.EventHandler(this.btnOk1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(809, 208);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1439, 624);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnOk3);
+            this.tabPage1.Controls.Add(this.btnOk2);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnOk1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtArray);
+            this.tabPage1.Controls.Add(this.lblResult);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1431, 598);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnOk3
+            // 
+            this.btnOk3.Location = new System.Drawing.Point(559, 442);
+            this.btnOk3.Name = "btnOk3";
+            this.btnOk3.Size = new System.Drawing.Size(75, 23);
+            this.btnOk3.TabIndex = 8;
+            this.btnOk3.Text = "button3";
+            this.btnOk3.UseVisualStyleBackColor = true;
+            this.btnOk3.Click += new System.EventHandler(this.btnOk3_Click);
+            // 
+            // btnOk2
+            // 
+            this.btnOk2.Location = new System.Drawing.Point(395, 442);
+            this.btnOk2.Name = "btnOk2";
+            this.btnOk2.Size = new System.Drawing.Size(75, 23);
+            this.btnOk2.TabIndex = 7;
+            this.btnOk2.Text = "button2";
+            this.btnOk2.UseVisualStyleBackColor = true;
+            this.btnOk2.Click += new System.EventHandler(this.btnOk2_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1431, 598);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1463, 637);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtArray;
+        private System.Windows.Forms.Button btnOk1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnOk3;
+        private System.Windows.Forms.Button btnOk2;
+    }
+}
+
---
+++ laba4.1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba4._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int CountNegatives(double[] numbers)
+        {
+            return numbers.Count(x => x < 0);
+        }
+
+        private double SumAfterMinModulus(double[] numbers)
+        {
+            double minModulus = numbers.Select(Math.Abs).Min();
+            int minIndex = Array.IndexOf(numbers, minModulus);
+            double sum = numbers.Skip(minIndex + 1).Sum();
+            return sum;
+        }
+
+        private double[] ReplaceNegativesWithSquares(double[] numbers)
+        {
+            double[] squaredNumbers = numbers.Select(x => x < 0 ? x * x : x).ToArray();
+            Array.Sort(squaredNumbers);
+            return squaredNumbers;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOk1_Click(object sender, EventArgs e)
+        {
+            if (txtArray.TextLength != 0)
+            {
+                string[] numbersStr = txtArray.Text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                double[] numbers = Array.ConvertAll(numbersStr, double.Parse);
+
+                int negativeCount = CountNegatives(numbers);
+
+                lblResult.Text = negativeCount.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input data!");
+            }
+        }
+
+        private void btnOk2_Click(object sender, EventArgs e)
+        {
+            if (txtArray.TextLength != 0)
+            {
+                string[] numbersStr = txtArray.Text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                double[] numbers = Array.ConvertAll(numbersStr, double.Parse);
+
+                double sumAfterMinModulus = SumAfterMinModulus(numbers);
+
+                lblResult.Text = sumAfterMinModulus.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input data!");
+            }
+        }
+
+        private void btnOk3_Click(object sender, EventArgs e)
+        {
+            if (txtArray.TextLength != 0)
+            {
+                string[] numbersStr = txtArray.Text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                double[] numbers = Array.ConvertAll(numbersStr, double.Parse);
+
+                double[] squaredNumbers = ReplaceNegativesWithSquares(numbers);
+
+                lblResult.Text = squaredNumbers.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input data!");
+            }
+        }
+    }
+}
