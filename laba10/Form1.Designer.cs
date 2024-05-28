f3c69ddf4995db6dd9e966f9e21e8865724b8817
skarishin12345@gmail.com <skarishin12345@gmail.com>
2024-05-28 20:45:44 +0300

    laba10

---
+++ laba10/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace laba10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(32, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(295, 327);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(351, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(317, 327);
+            this.panel2.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(725, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(384, 327);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(32, 398);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(295, 23);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "button1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(351, 398);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(317, 23);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "button2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(725, 398);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(384, 23);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "button3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(32, 445);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(295, 23);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "button4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(351, 445);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(317, 23);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "button5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(725, 445);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(384, 23);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "button6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(32, 529);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(1077, 23);
+            this.btn7.TabIndex = 9;
+            this.btn7.Text = "button7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(32, 587);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(1077, 23);
+            this.btn8.TabIndex = 10;
+            this.btn8.Text = "button8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 650);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+    }
+}
+
---
+++ laba10/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba10
+{
+    public partial class Form1 : Form
+    {
+        private Thread thread1;
+        private Thread thread2;
+        private Thread thread3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            thread1 = new Thread(new ThreadStart(DrawRectangles));
+            thread2 = new Thread(new ThreadStart(DrawEllipses));
+            thread3 = new Thread(new ThreadStart(GenerateRandomNumbers));
+        }
+
+        private void DrawRectangles()
+        {
+            try
+            {
+                Random rnd = new Random();
+                Graphics g = panel1.CreateGraphics();
+                while (true)
+                {
+                    Thread.Sleep(40);
+                    g.DrawRectangle(Pens.Pink, rnd.Next(panel1.Width), rnd.Next(panel1.Height), 50, 30);
+                }
+            }
+            catch (ThreadAbortException) { }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DrawEllipses()
+        {
+            try
+            {
+                Random rnd = new Random();
+                Graphics g = panel2.CreateGraphics();
+                while (true)
+                {
+                    Thread.Sleep(40);
+                    g.DrawEllipse(Pens.Blue, rnd.Next(panel2.Width), rnd.Next(panel2.Height), 50, 30);
+                }
+            }
+            catch (ThreadAbortException) { }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void GenerateRandomNumbers()
+        {
+            try
+            {
+                Random rnd = new Random();
+                while (true)
+                {
+                    Thread.Sleep(100);
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        richTextBox1.AppendText(rnd.Next().ToString() + Environment.NewLine);
+                    });
+                }
+            }
+            catch (ThreadAbortException) { }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            thread1.Start();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            thread1.Abort();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            thread2.Start();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            thread2.Abort();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            thread3.Start();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            thread3.Abort();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btn1.Text = "start";
+            btn2.Text = "stop";
+            btn3.Text = "start";
+            btn4.Text = "stop";
+            btn5.Text = "start";
+            btn6.Text = "stop";
+            btn7.Text = "total start";
+            btn8.Text = "total stop";
+        }
+          
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            thread1.Abort();
+            thread2.Abort();
+            thread3.Abort();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (thread1 != null && thread1.IsAlive) thread1.Abort();
+            if (thread2 != null && thread2.IsAlive) thread2.Abort();
+            if (thread3 != null && thread3.IsAlive) thread3.Abort();
+        }
+    }
+}
