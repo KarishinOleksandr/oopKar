b3cf26f483e90b49ff26fcc1c3ab414253e09f92
skarishin12345@gmail.com <skarishin12345@gmail.com>
2024-06-18 21:40:37 +0300

    laba19

---
+++ laba19/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace laba19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtProcessor = new System.Windows.Forms.TextBox();
+            this.txtManufacturer = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtVideoCard = new System.Windows.Forms.TextBox();
+            this.txtDriverVersion = new System.Windows.Forms.TextBox();
+            this.txtAdapterRAM = new System.Windows.Forms.TextBox();
+            this.txtDVDName = new System.Windows.Forms.TextBox();
+            this.txtDVDDrive = new System.Windows.Forms.TextBox();
+            this.txtDiskDrive = new System.Windows.Forms.TextBox();
+            this.txtDiskSize = new System.Windows.Forms.TextBox();
+            this.txtMotherboard = new System.Windows.Forms.TextBox();
+            this.txtNetworkAdapter = new System.Windows.Forms.TextBox();
+            this.txtVideoProcessor = new System.Windows.Forms.TextBox();
+            this.txtBIOS = new System.Windows.Forms.TextBox();
+            this.btnGetInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Процесор";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Виробник";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Опис";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Відеокарта";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Версія драйверу";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Об\'єм памєяті (в байтах)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 258);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Назва DVD";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 296);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Буква DVD";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 335);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Жорсткий диск";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(13, 377);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(89, 13);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Об\'єм (в байтах)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 413);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(107, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Материнська плата";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(13, 444);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(123, 13);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "Мережеве обладнання";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(9, 153);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(82, 13);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "Відеопроцесор";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(13, 484);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(32, 13);
+            this.label14.TabIndex = 12;
+            this.label14.Text = "BIOS";
+            // 
+            // txtProcessor
+            // 
+            this.txtProcessor.Location = new System.Drawing.Point(171, 9);
+            this.txtProcessor.Name = "txtProcessor";
+            this.txtProcessor.Size = new System.Drawing.Size(410, 20);
+            this.txtProcessor.TabIndex = 14;
+            // 
+            // txtManufacturer
+            // 
+            this.txtManufacturer.Location = new System.Drawing.Point(171, 40);
+            this.txtManufacturer.Name = "txtManufacturer";
+            this.txtManufacturer.Size = new System.Drawing.Size(410, 20);
+            this.txtManufacturer.TabIndex = 15;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(171, 76);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(410, 20);
+            this.txtDescription.TabIndex = 16;
+            // 
+            // txtVideoCard
+            // 
+            this.txtVideoCard.Location = new System.Drawing.Point(171, 112);
+            this.txtVideoCard.Name = "txtVideoCard";
+            this.txtVideoCard.Size = new System.Drawing.Size(410, 20);
+            this.txtVideoCard.TabIndex = 17;
+            // 
+            // txtDriverVersion
+            // 
+            this.txtDriverVersion.Location = new System.Drawing.Point(171, 182);
+            this.txtDriverVersion.Name = "txtDriverVersion";
+            this.txtDriverVersion.Size = new System.Drawing.Size(410, 20);
+            this.txtDriverVersion.TabIndex = 18;
+            // 
+            // txtAdapterRAM
+            // 
+            this.txtAdapterRAM.Location = new System.Drawing.Point(171, 217);
+            this.txtAdapterRAM.Name = "txtAdapterRAM";
+            this.txtAdapterRAM.Size = new System.Drawing.Size(410, 20);
+            this.txtAdapterRAM.TabIndex = 19;
+            // 
+            // txtDVDName
+            // 
+            this.txtDVDName.Location = new System.Drawing.Point(171, 258);
+            this.txtDVDName.Name = "txtDVDName";
+            this.txtDVDName.Size = new System.Drawing.Size(410, 20);
+            this.txtDVDName.TabIndex = 20;
+            // 
+            // txtDVDDrive
+            // 
+            this.txtDVDDrive.Location = new System.Drawing.Point(171, 292);
+            this.txtDVDDrive.Name = "txtDVDDrive";
+            this.txtDVDDrive.Size = new System.Drawing.Size(410, 20);
+            this.txtDVDDrive.TabIndex = 21;
+            // 
+            // txtDiskDrive
+            // 
+            this.txtDiskDrive.Location = new System.Drawing.Point(171, 331);
+            this.txtDiskDrive.Name = "txtDiskDrive";
+            this.txtDiskDrive.Size = new System.Drawing.Size(410, 20);
+            this.txtDiskDrive.TabIndex = 22;
+            // 
+            // txtDiskSize
+            // 
+            this.txtDiskSize.Location = new System.Drawing.Point(171, 373);
+            this.txtDiskSize.Name = "txtDiskSize";
+            this.txtDiskSize.Size = new System.Drawing.Size(410, 20);
+            this.txtDiskSize.TabIndex = 23;
+            // 
+            // txtMotherboard
+            // 
+            this.txtMotherboard.Location = new System.Drawing.Point(171, 413);
+            this.txtMotherboard.Name = "txtMotherboard";
+            this.txtMotherboard.Size = new System.Drawing.Size(410, 20);
+            this.txtMotherboard.TabIndex = 24;
+            // 
+            // txtNetworkAdapter
+            // 
+            this.txtNetworkAdapter.Location = new System.Drawing.Point(171, 444);
+            this.txtNetworkAdapter.Name = "txtNetworkAdapter";
+            this.txtNetworkAdapter.Size = new System.Drawing.Size(410, 20);
+            this.txtNetworkAdapter.TabIndex = 25;
+            // 
+            // txtVideoProcessor
+            // 
+            this.txtVideoProcessor.Location = new System.Drawing.Point(171, 149);
+            this.txtVideoProcessor.Name = "txtVideoProcessor";
+            this.txtVideoProcessor.Size = new System.Drawing.Size(410, 20);
+            this.txtVideoProcessor.TabIndex = 26;
+            // 
+            // txtBIOS
+            // 
+            this.txtBIOS.Location = new System.Drawing.Point(171, 480);
+            this.txtBIOS.Multiline = true;
+            this.txtBIOS.Name = "txtBIOS";
+            this.txtBIOS.Size = new System.Drawing.Size(410, 91);
+            this.txtBIOS.TabIndex = 27;
+            // 
+            // btnGetInfo
+            // 
+            this.btnGetInfo.Location = new System.Drawing.Point(16, 577);
+            this.btnGetInfo.Name = "btnGetInfo";
+            this.btnGetInfo.Size = new System.Drawing.Size(565, 63);
+            this.btnGetInfo.TabIndex = 28;
+            this.btnGetInfo.Text = "Отримати інформацію";
+            this.btnGetInfo.UseVisualStyleBackColor = true;
+            this.btnGetInfo.Click += new System.EventHandler(this.btnGetInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 652);
+            this.Controls.Add(this.btnGetInfo);
+            this.Controls.Add(this.txtBIOS);
+            this.Controls.Add(this.txtVideoProcessor);
+            this.Controls.Add(this.txtNetworkAdapter);
+            this.Controls.Add(this.txtMotherboard);
+            this.Controls.Add(this.txtDiskSize);
+            this.Controls.Add(this.txtDiskDrive);
+            this.Controls.Add(this.txtDVDDrive);
+            this.Controls.Add(this.txtDVDName);
+            this.Controls.Add(this.txtAdapterRAM);
+            this.Controls.Add(this.txtDriverVersion);
+            this.Controls.Add(this.txtVideoCard);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtManufacturer);
+            this.Controls.Add(this.txtProcessor);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtProcessor;
+        private System.Windows.Forms.TextBox txtManufacturer;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtVideoCard;
+        private System.Windows.Forms.TextBox txtDriverVersion;
+        private System.Windows.Forms.TextBox txtAdapterRAM;
+        private System.Windows.Forms.TextBox txtDVDName;
+        private System.Windows.Forms.TextBox txtDVDDrive;
+        private System.Windows.Forms.TextBox txtDiskDrive;
+        private System.Windows.Forms.TextBox txtDiskSize;
+        private System.Windows.Forms.TextBox txtMotherboard;
+        private System.Windows.Forms.TextBox txtNetworkAdapter;
+        private System.Windows.Forms.TextBox txtVideoProcessor;
+        private System.Windows.Forms.TextBox txtBIOS;
+        private System.Windows.Forms.Button btnGetInfo;
+    }
+}
+
