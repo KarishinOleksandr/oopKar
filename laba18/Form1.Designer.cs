ace618c66a261d4c5e64556a86f6dde72c6e9005
skarishin12345@gmail.com <skarishin12345@gmail.com>
2024-06-18 17:17:20 +0300

    laba18

---
+++ laba18/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace laba18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewProcesses = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcesses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewProcesses
+            // 
+            this.dataGridViewProcesses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProcesses.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewProcesses.Name = "dataGridViewProcesses";
+            this.dataGridViewProcesses.Size = new System.Drawing.Size(476, 388);
+            this.dataGridViewProcesses.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 412);
+            this.Controls.Add(this.dataGridViewProcesses);
+            this.Name = "Form1";
+            this.Text = "laba18";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcesses)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewProcesses;
+    }
+}
+
